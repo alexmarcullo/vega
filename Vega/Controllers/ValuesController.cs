0b0a44f3b480a9ba98a0c67bb6003fbac7369d46
Alex Gonçalves <alex.marcullo@hotmail.com>
2017-09-26 00:42:37 -0300

    Interacao com o banco de dados

--- Vega/Controllers/ValuesController.cs
+++ Vega/Controllers/ValuesController.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace Vega.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+
+        private IConfiguration _config;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            using(SqlConnection conexao = new SqlConnection(_config.GetConnectionString("CnnStr")))
+            {
+                return conexao.Query<string>("");
+            }
         }
 
         // GET api/values/5
